6ce9055d25f92eead558c971c32e59e4c5a39b5b
shashwat <shashwat1461.cse18@chitkara.edu.in>
2025-03-15 14:25:30 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const PORT = 3000 || process.env.PORT;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(3000, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
