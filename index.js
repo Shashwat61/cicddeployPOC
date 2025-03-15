ba5481016683e7d2794071ce39f76e4d63009aee
shashwat <shashwat1461.cse18@chitkara.edu.in>
2025-03-15 16:54:42 +0530

    updated index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 3000 || process.env.PORT;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World on update');
+    res.send('Hello World on updated again');
 })
 
 app.listen(3000, () => {
